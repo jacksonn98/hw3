54d1779643cede7e3bfd59c171de5f47b24645d8
jacksonn98 <jackson.tirmonia@kellogg.northwestern.edu>
2026-02-24 01:27:31 +0000

    Working from posts example

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources "places"
+  # get("/posts", {:controller => "posts", :action => "index"})
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
