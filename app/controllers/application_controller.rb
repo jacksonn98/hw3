93966945e7b97975ad2de2ee0e59a047b8db2a64
jacksonn98 <jackson.tirmonia@kellogg.northwestern.edu>
2026-02-24 02:11:00 +0000

    places controller added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,2 @@
 class ApplicationController < ActionController::Base
-   def index
-    # find all places
-    @places = Place.all
-    # render places/index view
-  end
-
-  def new
-    # render places/new view with new place form
-  end
-
-  def create
-    place = Place.new
-    place["title"] = params["title"]
-    place["description"] = params["description"]
-    place["date"] = params["image"]
-    p.save
-    redirect_to "/posts"
-  end
 end
---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,20 @@
+class PlacesController < ApplicationController
+   def index
+    # find all places
+    @places = Place.all
+    # render places/index view
+  end
+
+  def new
+    # render places/new view with new place form
+  end
+
+  def create
+    place = Place.new
+    place["title"] = params["title"]
+    place["description"] = params["description"]
+    place["date"] = params["image"]
+    p.save
+    redirect_to "/posts"
+  end
+end
