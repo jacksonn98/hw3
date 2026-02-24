817c4fe4140dd8c434c5ed95517cebfa6de98091
jacksonn98 <jackson.tirmonia@kellogg.northwestern.edu>
2026-02-24 03:30:00 +0000

    Set up places page & form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,22 +5,18 @@ class PlacesController < ApplicationController
     # render places/index view
   end
 
-    def show
-    # find a Company
-    @company = Company.find_by({ "id" => params["id"]})
-    # render companies/show view with details about Company
+  def show
+   @place = Place.find_by({ "id" => params["id"] })
   end
 
   def new
-    # render places/new view with new place form
+    render :template => "places/new"
   end
 
   def create
-    place = Place.new
-    place["title"] = params["title"]
-    place["description"] = params["description"]
-    place["date"] = params["image"]
-    p.save
-    redirect_to "/posts"
+@place = Place.new
+  @place["name"] = params["name"]
+  @place.save
+  redirect_to "/places"
   end
 end
